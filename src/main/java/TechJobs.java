aed2e64cd80909ed423c6abd9fba8ca922e4122d
OmarAlsadeq <109781174+OmarAlsadeq@users.noreply.github.com>
2022-11-20 16:55:06 -0600

    "first commit"

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -120,6 +120,8 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
+        String asertisk = "*****";
+
         System.out.println("printJobs is not implemented yet");
     }
 }
